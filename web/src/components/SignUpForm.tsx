15fdd5a8d28bc5c2772b24731145b4b66217b75a
anastasia-starostina <anastasia.st.stern@gmail.com>
2023-04-20 17:17:21 +0100

    install axios and set up axios configuration as well as onChange handlers in SignUpForm component

--- web/src/components/SignUpForm.tsx
+++ web/src/components/SignUpForm.tsx
@@ -1,7 +1,38 @@
+import axios from 'axios';
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
+
 interface SignUpFormProps {}
 
 function LoginForm({}: SignUpFormProps) {
+  const [email, setEmail] = useState('');
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [register, setRegister] = useState(false);
+
+  //set axios configuration
+  const configuration = {
+    method: 'post',
+    url: import.meta.env.VITE_API_URL + 'user/register',
+    data: {
+      email,
+      password,
+    },
+  };
+  // make the API call
+  axios(configuration)
+    .then((result) => {
+      console.log(result);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    alert('Submited');
+  };
+
   return (
     <section className="flex flex-col mt-20 items-center justify-center">
       <div className="text-left w-96 ">
@@ -16,21 +47,27 @@ function LoginForm({}: SignUpFormProps) {
           <Link to="/login">Already have an online account? Log in here.</Link>
         </p>
       </div>
-      <form className="">
+      <form onSubmit={(e) => handleSubmit(e)}>
         <div className="flex flex-col w-96 py-4 space-y-4 rounded-md ">
           <input
             type="username"
             placeholder="Username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
             className="p-2 border border-medium_grey rounded-md hover:border-dark_grey"
           />
           <input
             type="email"
             placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
             className="p-2 border border-medium_grey rounded-md hover:border-dark_grey"
           />
           <input
             type="password"
             placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
             className="p-2 border  border-medium_grey rounded-md hover:border-dark_grey"
           />
           <input
@@ -38,9 +75,11 @@ function LoginForm({}: SignUpFormProps) {
             placeholder="Confirm Password"
             className="p-2 border  border-medium_grey rounded-md hover:border-dark_grey"
           />
-          <small className="cursor-pointer text-orange font-bold text-right hover:underline">
-          </small>
-          <button className="bg-orange tracking-wider p-4 font-bold bg-blue-500 rounded-md text-white hover:bg-dark_orange active:bg-dark_orange">
+          <small className="cursor-pointer text-orange font-bold text-right hover:underline"></small>
+          <button
+            onClick={(e) => handleSubmit(e)}
+            className="bg-orange tracking-wider p-4 font-bold bg-blue-500 rounded-md text-white hover:bg-dark_orange active:bg-dark_orange"
+          >
             SIGN UP
           </button>
         </div>
