cf248be4c7614943400550577c0a5dc1a94fda82
anastasia-starostina <anastasia.st.stern@gmail.com>
2023-04-22 17:22:17 +0100

    replace bcrypt with bcryptjs

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const cors = require("cors");
 const jwt = require("jsonwebtoken");
 
